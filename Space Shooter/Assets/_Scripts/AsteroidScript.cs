3de886a7c87374d7dcaf6cd5a2fd73396649d1e6
German Pereyra <german.f.pereyra@gmail.com>
2015-10-15 17:41:39 -0300

    CREATING HAZARDS

---
+++ Space Shooter/Assets/_Scripts/AsteroidScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidScript : MonoBehaviour {
+
+	public float tumble;
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+
+}
---
+++ Space Shooter/Assets/_Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log (other.name);
+		if (other.tag == "JailTag") {
+			return;
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
