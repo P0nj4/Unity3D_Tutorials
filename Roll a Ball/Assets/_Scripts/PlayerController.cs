142efc638260a3773da46b4fd1f6745479e319bd
German Pereyra <german.f.pereyra@gmail.com>
2015-10-15 14:03:07 -0300

    first commit

---
+++ Roll a Ball/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text finishText;
+
+	private Rigidbody rb;
+	private int count;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		updateCount();
+	}
+
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 vec = new Vector3 (moveHorizontal * 1, 0.0f, moveVertical * 1);
+		rb.AddForce (vec * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count+=1;
+			updateCount();
+		}
+	}
+
+	void updateCount(){
+		this.countText.text = "Score: " + (this.count * 10).ToString();
+		if (this.count == 12) {
+			this.finishText.gameObject.SetActive(true);
+		}
+	}
+}
---
+++ Space Shooter/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xmin, xmax, zmin, zmax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Boundary boundary;
+	private Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		this.rigidBody.velocity = new Vector3 (moveHorizontal * speed, 0.0f, moveVertical * speed);
+
+		rigidBody.position = new Vector3 (
+			Mathf.Clamp(this.rigidBody.position.x,this.boundary.xmin, this.boundary.xmax),
+			0.0f,
+			Mathf.Clamp(this.rigidBody.position.z,this.boundary.zmin, this.boundary.zmax)
+		);
+
+		rigidBody.rotation = Quaternion.Euler (0.0f, 0.0f, this.rigidBody.velocity.x * -3);
+	}
+}
