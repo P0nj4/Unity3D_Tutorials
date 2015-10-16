ce9ef9a05117beb5a8ee2020b2057d348232d41d
German Pereyra <german.f.pereyra@gmail.com>
2015-10-16 14:01:43 -0300

    explotions

--- Space Shooter/Assets/_Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/_Scripts/DestroyByContact.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject expotion;
+	public GameObject playerExpotion;
+
 	void OnTriggerEnter(Collider other) {
 		Debug.Log (other.name);
 		if (other.tag == "JailTag") {
@@ -10,5 +13,9 @@ public class DestroyByContact : MonoBehaviour {
 		}
 		Destroy(other.gameObject);
 		Destroy(gameObject);
+		Instantiate (expotion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExpotion, other.transform.position, other.transform.rotation);
+		}
 	}
 }
