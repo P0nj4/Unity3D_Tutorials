941af74235a0c54f41bf31ecab9165045a50dd88
German Pereyra <german.f.pereyra@gmail.com>
2015-10-16 14:35:58 -0300

    Spawn waves

---
+++ Space Shooter/Assets/_Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+
+	public GameObject hazzard;
+	public Vector3 spawnValue;
+	public float spawnWait;
+	public float startWait;
+
+	IEnumerator spawnWaves() {
+		while (true) {
+			yield return new WaitForSeconds (startWait);
+
+			for (int i = 0; i < 10; i++) {
+				Vector3 position = new Vector3 (Random.Range (spawnValue.x * -1, spawnValue.x), spawnValue.y, spawnValue.z);
+				Quaternion rotation = Quaternion.identity;
+				Instantiate (hazzard, position, rotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (spawnWaves ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
