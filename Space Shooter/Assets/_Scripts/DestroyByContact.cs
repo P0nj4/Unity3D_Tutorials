edfc888cf3eaa2fca5047bf4b93dad3479fc7d1f
German Pereyra <german.f.pereyra@gmail.com>
2015-10-16 15:20:31 -0300

    explotion audio

--- Space Shooter/Assets/_Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/_Scripts/DestroyByContact.cs
@@ -7,14 +7,16 @@ public class DestroyByContact : MonoBehaviour {
 	public GameObject playerExpotion;
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log (other.name);
+		//Debug.Log (other.name);
 		if (other.tag == "JailTag") {
 			return;
 		}
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 		Instantiate (expotion, transform.position, transform.rotation);
-		if (other.tag == "Player") {
+		Debug.Log (other.tag);
+		if (other.tag == "Player1") {
+			Debug.Log ("lalala");
 			Instantiate (playerExpotion, other.transform.position, other.transform.rotation);
 		}
 	}
